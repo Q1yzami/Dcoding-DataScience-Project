6ef24ed72d8529dc37504456b9e25b34c23af3d1
Q1yzami <123069230+Q1yzami@users.noreply.github.com>
2024-11-06 21:38:01 +0700

    Add files via upload
    update dashboar.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -80,7 +80,7 @@ def create_rfm_df(df):
     return rfm_df
 
 # Load cleaned data
-all_df = pd.read_csv("all_data.csv")
+all_df = pd.read_csv("dashboard/all_data.csv")
 
 datetime_col = ['order_purchase_timestamp', 'order_delivered_customer_date']
 all_df.sort_values(by='order_purchase_timestamp', inplace=True)
